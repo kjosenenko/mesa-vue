bbf7573be916a2608509961958a09cfebf192361
KJ Osenenko <kj@mac-kj.local>
2024-07-08 17:52:34 -0600

    add photo

--- src/views/Music.vue
+++ src/views/Music.vue
@@ -3,6 +3,7 @@
     <div className="container d-none d-lg-block" id="container-div">
       <div className="black center-text">
         <h1 className="monoton">LATEST RELEASES</h1>
+        <img src="../assets/IMG_8970.jpeg" className='photo mb-2' />
         <div className="music-container">
           <iframe width="560" height="315" src="https://www.youtube.com/embed/NtpqNEEa0E0?si=5TWxMIICvnVt1BaV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           <iframe width="560" height="315" src="https://www.youtube.com/embed/ZINV4btYxD8?si=9mKncz-rlnwlyy0W" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
@@ -14,6 +15,7 @@
     <div className="d-lg-none" id="mobile-container">
       <div className="black center-text">
         <h1 className="monoton">LATEST RELEASES</h1>
+        <img src="../assets/IMG_8970.jpeg" className='photo-mobile mb-2' />
         <div className="music-container">
           <a className="btn btn-dark" href='https://youtu.be/NtpqNEEa0E0?si=jUREBNAzAAXvC_kf'>Mesa Divide @ Chippers Lanes</a>
           <a className="btn btn-dark" href='https://youtu.be/ZINV4btYxD8?si=9mKncz-rlnwlyy0W'>Ripper (live) @ Chippers Lanes</a>
